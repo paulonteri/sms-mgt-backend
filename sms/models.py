909d283daddda1fb5d3df5656ca247ac51570c50
paulonteri <onteripaul@gmail.com>
2020-05-04 15:46:00 +0300

    Added simple messages models

--- sms/models.py
+++ sms/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class MessageInfo(models.Model):
+    """
+    general information about a group of SMS(s) is sent
+    created per Africa'sTalking send SMS API call
+    """
+    success = models.BooleanField(
+        default=False, editable=False)  # check whether message(s) sent successfully
+    message_text = models.CharField(
+        max_length=255, editable=False)  # actual sms messge text sent
+    africastalking_response = models.CharField(
+        max_length=255,  editable=False)  # response massage received from africastalking
+    # time message(s) finished sending
+    time_sent = models.DateTimeField(editable=False)
+    time_added = models.DateTimeField(
+        auto_now_add=True)  # when the message object is first created
+    time_last_edited = models.DateTimeField(
+        auto_now_add=True)
+
+    class Meta:
+        ordering = ["time_last_edited"]
+
+
+class Message(models.Model):
+    """
+    information about a single SMS sent to a single receiver
+    """
+    message_info = models.ForeignKey(
+        MessageInfo, on_delete=models.CASCADE, editable=False)
+    message_id = models.CharField(max_length=255, editable=False)
+    status_code = models.CharField(max_length=255, editable=False)
+    number = models.CharField(max_length=255, editable=False)
+    cost = models.CharField(max_length=255, editable=False)
+    status = models.CharField(max_length=255, editable=False)
+    time_added = models.DateTimeField(auto_now_add=True)
+    time_last_edited = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        order_with_respect_to = 'message_info'
