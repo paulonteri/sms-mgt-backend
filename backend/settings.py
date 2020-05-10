41520b35e53753f515b3d144dd10e43c2cb8a666
paulonteri <onteripaul@gmail.com>
2020-05-10 11:46:20 +0300

    Added cors settings

--- backend/settings.py
+++ backend/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'knox',
     'accounts',
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware', # cors
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,5 @@ REST_FRAMEWORK = {
 # Africa's talking
 AFRICASTALKING_USERNAME = get_env_variable("AFRICASTALKING_USERNAME")
 AFRICASTALKING_API_KEY = get_env_variable("AFRICASTALKING_API_KEY")
+
+CORS_ORIGIN_ALLOW_ALL = True
