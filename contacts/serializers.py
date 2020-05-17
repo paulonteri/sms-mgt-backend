db3adb1967ae5690e7112000d67af5201cecca6d
paulonteri <onteripaul@gmail.com>
2020-05-17 13:49:12 +0300

    fix serializer bug

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -22,4 +22,5 @@ class ContactSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
         fields = ["first_name", "last_name", "other_name", "phone_number", "email",
-                  "gender", "is_active", "time_created", "last_updated", "tags"]
+                  "gender", "is_active",  "time_added",
+                  "time_last_edited", "tags"]
