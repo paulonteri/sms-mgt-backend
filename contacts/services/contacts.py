355c001000ebef48d52b9d5222ee6da223cd4cbd
paulonteri <onteripaul@gmail.com>
2020-05-18 16:16:56 +0300

    Changed get contacts to handle lists

--- contacts/services/contacts.py
+++ contacts/services/contacts.py
@@ -21,22 +21,46 @@ def return_contact_numbers_with_tag(tags):
 
     # get contact numbers
     phone_nos = []
-    for tag in tags:
-        try:
-            phone_nos.extend(Contact.objects.filter(contacttag=tag).values_list("phone_number", flat=True))
-        except:
-            pass
 
+    try:
+        phone_nos.extend(Contact.objects.filter(contacttag__in=tags).values_list("phone_number", flat=True))
+    except Exception as e:
+        raise Exception(e)
     if not (len(phone_nos) > 0):
         raise Exception("No contacts Found")
     else:
         return phone_nos
 
 
-def get_contact_number(contact):
+def get_contact_number(contacts):
     # get a single contacts phone number
+    # try:
+    #     cont = [Contact.objects.get(pk=contact).phone_number, ]
+    #     return cont
+    # except:
+    #     raise Exception("Contact does not exist")
+
+    """
+    :return: the phone numbsers of contacts witha certain tag
+    """
+    # verify data
+    if not (len(contacts) > 0):
+        raise Exception("Contacts cannot be blank")
+    if not isinstance(contacts, list):
+        raise Exception("Incorrect data type, the Contacts value should be a list")
+    for q in contacts:
+        if not (isinstance(q, int)):
+            raise Exception("Incorrect data type, the individual contacts should be integers")
+
+    # get contact numbers
+    phone_nos = []
     try:
-        cont = [Contact.objects.get(pk=contact).phone_number, ]
-        return cont
-    except:
-        raise Exception("Contact does not exist")
+        phone_nos.extend(Contact.objects.filter(pk__in=contacts).values_list("phone_number", flat=True))
+    except Exception as e:
+        raise Exception(e)
+
+    if not (len(phone_nos) > 0):
+        raise Exception("No contacts Found")
+    else:
+        print(phone_nos)
+        return phone_nos
