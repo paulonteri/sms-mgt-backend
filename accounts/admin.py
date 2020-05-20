a6cfa1d4fb8781f6d537851868d2c080ac8d034e
paulonteri <onteripaul@gmail.com>
2020-05-20 14:12:08 +0300

    Added UserInformation url and to tha admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, UserInformation
 
 # Register your models here.
+
+admin.register(User)
+
+
+@admin.register(UserInformation)
+class UserInfoAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        "time_added",
+        "time_last_edited",
+    ]
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from knox import views as knox_views
 
-from .api import RegisterAPI, LoginAPI, UserAPI
+from .api import RegisterAPI, LoginAPI, UserAPI, UserInformationAPI
 
 urlpatterns = [
     path('user', UserAPI.as_view()),
+    path('userinfo', UserInformationAPI.as_view()),
     path('login', LoginAPI.as_view()),
     path('register', RegisterAPI.as_view()),
     path('logout', knox_views.LogoutView.as_view(), name='knox_logout'),
