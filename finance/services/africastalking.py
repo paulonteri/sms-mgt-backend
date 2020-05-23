08b79fce0bcdc089cc2212e0db127034a06f4240
paulonteri <onteripaul@gmail.com>
2020-05-23 11:00:37 +0300

    get africastalking balance

---
+++ finance/services/africastalking.py
@@ -0,0 +1,25 @@
+import africastalking
+import requests
+
+from django.conf import settings
+
+# Initialize SDK
+username = settings.AFRICASTALKING_USERNAME
+apikey = settings.AFRICASTALKING_API_KEY
+africastalking.initialize(username, apikey)
+app = africastalking.Application
+
+
+def get_balance():
+    try:
+        response = app.fetch_application_data()
+        string_amount = response["UserData"]["balance"].replace('KES ', '')
+        int_amount = float(string_amount)
+        print(int_amount)
+    except requests.exceptions.ConnectionError:
+        raise Exception("Network Connection Error")
+    except requests.HTTPError:
+        raise Exception("HTTP Network Error")
+    except Exception as e:
+        # save the sms info to the db
+        raise e
