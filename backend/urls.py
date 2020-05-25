2d557f82f18ab5e16a88c8609988cea60adf3e55
paulonteri <onteripaul@gmail.com>
2020-05-25 15:41:32 +0300

    Added ping

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def pong(request):
+    return HttpResponse("pong")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/ping/', pong),
     path('api/v1/auth/', include('accounts.urls')),
     path('api/v1/sms/', include('sms.urls')),
     path('api/v1/contacts/', include('contacts.urls')),
