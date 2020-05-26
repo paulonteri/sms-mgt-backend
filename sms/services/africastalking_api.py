fe3b1517cb3bd6862790ad0a038820d425486855
paulonteri <onteripaul@gmail.com>
2020-05-26 09:54:41 +0300

    save number while sending SMS

--- sms/services/africastalking_api.py
+++ sms/services/africastalking_api.py
@@ -52,6 +52,7 @@ def send_sms(user, message, recipients):
                         contact=contact_list.get(phone_number__exact=i["number"]),
                         cost=i["cost"],
                         status_code=i["statusCode"],
+                        phone=i["number"]
                     ))
             else:
                 # create & save contact
@@ -69,6 +70,7 @@ def send_sms(user, message, recipients):
                         message_id=i["messageId"],
                         contact=contact_obj,
                         cost=i["cost"],
+                        phone=i["number"],
                         status_code=i["statusCode"], ))
 
         # save the list objects into the database in one query
