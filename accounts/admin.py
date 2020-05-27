394015f5ca24757ae07d8a616d89579a05045007
paulonteri <onteripaul@gmail.com>
2020-05-27 09:59:41 +0300

    Added permissions to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from .models import User, UserInformation
+from django.contrib.auth.models import Permission, Group
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Permission)
 
 
 @admin.register(UserInformation)
