5e63da57664af450f6f55ab55a9f4b358fe6fbcd
paulonteri <onteripaul@gmail.com>
2020-05-28 17:33:14 +0300

    CONN_MAX_AGE

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -22,7 +22,7 @@ DATABASES = {
         'PASSWORD': get_env_variable("DB_PASSWORD"),
         'HOST': get_env_variable("DB_HOST"),
         'PORT': get_env_variable("DB_PORT"),
-        'CONN_MAX_AGE': 20,
+        'CONN_MAX_AGE': 10,
     }
 }
 
